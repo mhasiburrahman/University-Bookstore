28388bf4bde206e775a0d888dd008a9bc99dc705
Hasibur Rahman <mhasiburrahman01@gmail.com>
2021-12-08 00:41:50 +0600

    nothing changed

--- Dashboard.cs
+++ Dashboard.cs
@@ -14,7 +14,8 @@ namespace University_Bookstore
    
     public partial class Dashboard : Form
     {
-        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\KAKON\Desktop\University-Bookstore\AdminDB\AdminloginDB.mdf;Integrated Security=True;Connect Timeout=30");
+        //SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\KAKON\Desktop\University-Bookstore\AdminDB\AdminloginDB.mdf;Integrated Security=True;Connect Timeout=30");
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\prott\Desktop\University-Bookstore\AdminDB\AdminloginDB.mdf;Integrated Security=True;Connect Timeout=30");
         public Dashboard()
         {
             InitializeComponent();
